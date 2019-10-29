2df7847415d3d46e9a24bb3ebb6c4de25ed4e425
Maria Christina B Duculan <mcduculan2011@gmail.com>
2019-10-29 17:21:55 +0000

    Automatically backed up by Learn

---
+++ hello_world.rb
@@ -0,0 +1,7 @@
+day_is_thursday = TRUE
+mom_is_not_home = TRUE
+# Ternary
+# Position 1 
+# Position 2 
+# Position 3
+do day_is_thursday && mom_is_not_home? "play scary video games" : "do homework" end
